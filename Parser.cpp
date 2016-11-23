a60faac6ec9f2e1202d437d7545f28f8eea247e0
Jason Lin <jason.lin.sjtu@gmail.com>
2016-11-23 01:47:49 +0000

    use treenode to save?

--- Parser.cpp
+++ Parser.cpp
@@ -4,12 +4,24 @@
 
 using namespace std;
 
+enum Op{
+	OP_AND,
+	OP_OR,
+	OP_NOT,
+	OP_IMPLY,
+};
+
+struct TreeNode{
+    Op op;
+    TreeNode* left;
+    int ls, le;
+    TreeNode* right;
+    int rs, re;
+}
+
 FolParser::FolParser(vector<string> folkb, vector<string> folq){
-    ifstream t(filename);
-    string str((istreambuf_iterator<char>(t)),
-                 istreambuf_iterator<char>());
-    t.close();
-    cout<<str<<endl;
+    
+    cout<<"parsing"<<endl;
     
     int lines_kb = str
 }
@@ -19,9 +31,18 @@ FolParser::~FolParser(){
 }
 
 KB FolParser::getKB(){
-    
+    return kb;
 }
 
 vector<Clause> FolParser::getQuery(){
-    
+    return queries;
+}
+
+bool FolParser::parse(string s){
+    int leftsemi = 0;
+    int len = s.size();
+    for(int i = 1; i< s.size(); ++i){
+        if()
+    }
+    return false;
 }
